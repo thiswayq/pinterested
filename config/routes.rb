86a5e6e4f8576aedb939392990c60cd93c07e73a
nicole <thiswayq@gmail.com>
2016-06-30 09:30:50 -0300

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'home#index'
   get 'about', to: 'home#about'
   # The priority is based upon order of creation: first created -> highest priority.
