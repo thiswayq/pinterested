8adbacd225a5f8f11a95ef3cfdaa9e02df6ba7d5
nicole <thiswayq@gmail.com>
2016-07-03 03:13:54 -0300

    readding s3 lol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,16 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
+
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.action_mailer.default_url_options = { host: 'pinterested-nicole.herokuapp.com' }
